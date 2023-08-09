7b243a81928223837d7d2df43b2d165b2cab30e8
Nawaraj karki <nawarajkarkee404@gmail.com>
2023-08-09 11:33:22 +0545

    Better import of the swap from comon

--- lab 1/bubbleSort.py
+++ lab 1/bubbleSort.py
@@ -1,3 +1,7 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from common import swap
 
 def bubbleSort(arr, n) -> list:
--- lab 1/selectionSort.py
+++ lab 1/selectionSort.py
@@ -1,4 +1,9 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from common import swap
+
 
 def selectionSort(arr, n) -> list:
     for i in range(n-1):
