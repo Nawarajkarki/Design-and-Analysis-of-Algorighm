23ad75dc84974c328a0f982ba410baaf2d165875
Nawaraj karki <nawarajkarkee404@gmail.com>
2023-08-09 12:07:45 +0545

    unn

--- lab 1/fibonacci Number.py
+++ lab 1/fibonacci Number.py
@@ -2,11 +2,11 @@
 def fibonacci(n) -> int:
     first_term = 0
     second_term = 1
-    if n > 2:
-        for i in range(3, n+1):
-            next_item = first_term + second_term
-            first_term = second_term
-            second_term = next_item
+
+    for i in range(3, n+1):
+        next_item = first_term + second_term
+        first_term = second_term
+        second_term = next_item
 
     return next_item
     
