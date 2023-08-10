685dec2732d1c5b4102ca4c61cdefbd668dd8345
Nawaraj karki <nawarajkarkee404@gmail.com>
2023-08-11 00:00:56 +0545

    .

--- Extra's/fibonacci Number.py
+++ Extra's/fibonacci Number.py
@@ -1,15 +1,25 @@
 
-def fibonacci(n) -> int:
-    first_term = 0
-    second_term = 1
+# def fibonacci(n) -> int:
+#     first_term = 0
+#     second_term = 1
+#     for i in range(3, n+1):
+#         next_item = first_term + second_term
+#         first_term = second_term
+#         second_term = next_item
+#     return next_item
+# n = int(input("Enter a number:- "))
+# print(fibonacci(n))
 
-    for i in range(3, n+1):
-        next_item = first_term + second_term
-        first_term = second_term
-        second_term = next_item
 
-    return next_item
-    
+#---------------- Using Recursion ---------------------------- #
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
 
 
 n = int(input("Enter a number:- "))
--- Extra's/fibonacci Sequence.py
+++ Extra's/fibonacci Sequence.py
@@ -1,29 +1,18 @@
 # ------------------- For fibonacci Sequence ---------------
-# def fibonacci(n) -> list:
-#     fibonacci_list = [0, 1]
-#     first_term = 0
-#     second_term = 1
-#     if n > 2:
-#         for i in range(3, n+1):
-#             next_item = first_term + second_term
-#             first_term = second_term
-#             second_term = next_item
-#             fibonacci_list.append(next_item)
-#     return fibonacci_list
-# n = int(input("Enter a number:- "))
-# print(fibonacci(n))
 
 
-#---------------- Using Recursion ---------------------------- #
-
-def fibonacci(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
-
-
+def fibonacci(n) -> list:
+    fibonacci_list = [0, 1]
+    first_term = 0
+    second_term = 1
+    if n > 2:
+        for i in range(3, n+1):
+            next_item = first_term + second_term
+            first_term = second_term
+            second_term = next_item
+            fibonacci_list.append(next_item)
+    return fibonacci_list
 n = int(input("Enter a number:- "))
 print(fibonacci(n))
+
+
