6810e7e2cd46b356df93c87031e1a1cf454ce116
Nawaraj karki <nawarajkarkee404@gmail.com>
2023-09-19 20:49:04 +0545

    Min-Max Finding using Python

---
+++ Python/Divide and Conquer/Min-Max Finding/divide-and-conquer-approach.py
@@ -0,0 +1,39 @@
+def min_max(my_list, p, r):
+
+    if (p == r):
+        max = min = my_list[p]
+    
+    elif (p == r-1):
+        if my_list[p] < my_list[r]:
+            max = my_list[r]
+            min = my_list[p]
+        else:
+            max = my_list[p]
+            min = my_list[r]
+    
+
+    else:
+        # Divide the problem in half
+        mid = (p + r) // 2   # Integer division
+
+        min1, max1 = min_max(my_list, p, mid)
+
+        min, max = min_max(my_list, mid+1, r)
+        if max1 > max:
+            max = max1
+        if min1 < min:
+            min = min1
+
+    
+    return min, max
+
+
+
+my_list = [5, 8, 7, 4, 2, 9, 12, 13, 11, 23, 1, -1, -23, 34, 545]
+l = len(my_list)
+
+min, max = min_max(my_list, 0, l-1)
+
+print(f"min = {min}, max = {max}")
+
+
---
+++ Python/Divide and Conquer/Min-Max Finding/iterative-method.py
@@ -0,0 +1,19 @@
+def min_max(my_list, len):
+    min = my_list[0]
+    max = my_list[0]
+
+    for i in range(1, len):
+        if my_list[i] > max:
+            max = my_list[i]
+        if my_list[i] < min:
+            min = my_list[i]
+    
+
+    return min, max
+
+
+my_list = [5, 8, 7, 4, 2, 9, 12, 13, 11, 23, 1, -1, -23, 34, 545]
+n = len(my_list)
+
+min, max = min_max(my_list, n)
+print(f"min = {min}, max = {max}")
